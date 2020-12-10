60e77ec97df0d47728ed6d5cbace63df6cace009
Alailson <aalailson3@gmail.com>
2020-12-10 04:25:32 +0000

    chore: add jest typeorm pg

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,9 @@
-export {}
+import express from 'express' 
+
+const app = express()
+
+
+
+app.listen("9090", ()=> {
+    console.log("listening on port 9090")
+})
---
+++ src/tests/sum.test.ts
@@ -0,0 +1,10 @@
+
+
+describe("should make a sum", () => {
+    test("should make some sum", () => {
+        expect(2).toBe(2)
+    })
+        test("should make some sum", () => {
+        expect(2).toBe(2)
+    })
+})
